a10c011876246a691fdd94e6d65ac80915a689d4
AlfredoBejarano <alfredocbejarano@gmail.com>
2020-03-13 11:12:26 -0600

    Create MainActivity.kt

--- app/src/main/java/me/alfredobejarano/brastlewarkarchitecturecomponents/MainActivity.kt
+++ app/src/main/java/me/alfredobejarano/brastlewarkarchitecturecomponents/MainActivity.kt
@@ -1,12 +1,21 @@
 package me.alfredobejarano.brastlewarkarchitecturecomponents
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.alfredobejarano.brastlewarkarchitecturecomponents.databinding.ActivityMainBinding
+import me.alfredobejarano.brastlewarkarchitecturecomponents.di.ViewModelFactory
+import me.alfredobejarano.brastlewarkarchitecturecomponents.viewmodel.GnomeListViewModel
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var viewModel: GnomeListViewModel
+    private lateinit var dataBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
